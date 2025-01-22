02beb1c59fa7fb045d5bfc245b11cbfa24e10f75
Waad Mawlood <w12345678996392@gmail.com>
2025-01-23 00:09:51 +0300

    Add pipMetadataToClearKeyNameId method to Helper class for metadata processing

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -2,6 +2,7 @@
 
 namespace Waad\Metadata\Helpers;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 
 class Helper
@@ -21,5 +22,18 @@ class Helper
         }
 
         return is_array($value) && empty($value);
+    }
+
+    public function pipMetadataToClearKeyNameId(array|Collection $metadata, string $keyNameId = 'id'): array
+    {
+        $metadata = $metadata instanceof Collection ? $metadata->toArray() : $metadata;
+        $firstItem = Arr::first($metadata);
+
+        // Check if metadata is nested (array of arrays)
+        if (filled($firstItem) && is_array($firstItem)) {
+            return array_map(fn ($item) => Arr::except($item, $keyNameId), $metadata);
+        }
+
+        return Arr::except($metadata, $keyNameId);
     }
 }
